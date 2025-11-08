92a4240968ef1967a4a1e2bb4ea6f2b46e7a13c2
ChristopherNesci <christopher.nesci@proton.me>
2025-11-08 18:34:56 +1100

    added string check and exit

--- password_checker.py
+++ password_checker.py
@@ -1,23 +1,62 @@
 import os
 
-def check_input():
-    user_input = input("Enter Password: ")
-    found_in = []
+def check_password():
+    while True:
+        print(
+        "Passwork Validator\n"
+        " Password should meet the following criteria:\n" 
+        " - Minimum length of 8 characters.\n"
+        " - Contains at least one uppercase letter.\n"
+        " - Contains at least one lowercase letter.\n"
+        " - Contains at least one digit.\n"
+        " - Contains at least one special character, eg !@#$%^&*().\n"
+        )
 
-    for filename in os.listdir("password_lists"):
-        filepath = os.path.join("password_lists", filename)
+        print("Type 'quit' to exit\n")
+                                             
 
-        if not os.path.isfile(filepath):
-            continue
+        user_input = input("Enter Password: ")
 
-        with open(filepath, encoding='utf-8', errors='ignore') as f:
-            file_contents = f.read()
-            if user_input in file_contents:
-                found_in.append(filename)
+        if user_input.lower() == 'quit':
+            print("Goodbye")
+            break
+
+        if len(user_input) < 8:
+            print("Password is too short!\n")
+        
+        if not any(letter.isupper() for letter in user_input):
+            print("You need at least 1 upper case letter!")
+        
+        if not any(letter.islower() for letter in user_input):
+            print("You need at least 1 lower case letter!")
+
+        if not any(letter.isdigit() for letter in user_input):
+            print("You need at least 1 digit!")
+        
+        special_chars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
+
+        if not any(letter in user_input for letter in special_chars):
+            print("You need at least 1 special character!")
+
+        found_in = []
+
+        for filename in os.listdir("password_lists"):
+            filepath = os.path.join("password_lists", filename)
+        
+            if not os.path.isfile(filepath):
+                continue
+
+            with open(filepath, encoding='utf-8', errors='ignore') as f:
+                file_contents = f.read()
+                if user_input in file_contents:
+                    found_in.append(filename)
     
-    if found_in:
-        print(f"'{user_input}' found in: {', '.join(found_in)}")
-    else:
-        print(f"'{user_input}' not found in any list")
+        if found_in:
+            print(f"'{user_input}' found in the following lists:\n")
+            print(*found_in, sep=', ')
+        else:
+            print(f"'{user_input}' not found in any list")
+        
+        print()
 
-check_input()
+check_password()
